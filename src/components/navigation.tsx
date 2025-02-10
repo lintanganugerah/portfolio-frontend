788c696ec40084942ea0495320a35d6f24ae1149
Adryan Putra Pratama <adryanpratama007@gmail.com>
2025-02-10 16:47:39 +0800

    adding store

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,3 +1,19 @@
 export const Navigation = () => {
-	return <div className=''>Navigation</div>;
+	return (
+		<div className='flex flex-row justify-around'>
+			<div className='w-1/2 flex  justify-center bg-red-50'>
+				<div className='w-full flex flex-row justify-around font-semibold'>
+					<div>Intro</div>
+					<div>Experience</div>
+					<div>Works</div>
+					<div>Certificate</div>
+				</div>
+			</div>
+			<div className='w-8 h-8 border rounded-full'>
+				<div className=''>
+					<img src='' alt='Profile Picture' />
+				</div>
+			</div>
+		</div>
+	);
 };
