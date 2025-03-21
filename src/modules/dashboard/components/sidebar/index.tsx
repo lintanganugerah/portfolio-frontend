5b6c91c32f51a1948fe300c6a6cc4cb7805cbf08
Adryan Putra Pratama <adryanpratama007@gmail.com>
2025-03-21 23:07:54 +0800

    reusable sidebar

--- src/modules/dashboard/components/sidebar/index.tsx
+++ src/modules/dashboard/components/sidebar/index.tsx
@@ -1,7 +1,19 @@
+import { MdSpaceDashboard } from "react-icons/md";
+import { SidebarComponent } from "./comps";
+
 export const Sidebar = () => {
 	return (
-		<div className='h-screen w-1/5 bg-white'>
-			<div></div>
+		<div className='h-screen w-1/5 flex flex-col space-y-4 bg-white'>
+			<div className='w-full flex justify-center p-4'>
+				<img className='h-32' src='/assets/logo.png' alt='' />
+			</div>
+			<div className='flex flex-col space-y-4 p-4'>
+				<SidebarComponent icon={MdSpaceDashboard} title='Dashboard' />
+				<SidebarComponent icon={MdSpaceDashboard} title='Dashboard' />
+				<SidebarComponent icon={MdSpaceDashboard} title='Dashboard' />
+				<SidebarComponent icon={MdSpaceDashboard} title='Dashboard' />
+				<SidebarComponent icon={MdSpaceDashboard} title='Dashboard' />
+			</div>
 		</div>
 	);
 };
